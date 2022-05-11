d933bfd49273127b41d6412631caee661a8354cb
GKrafft2 <guillaume.krafft2@gmail.com>
2022-05-11 15:20:49 +0200

    SLAM V2

--- projet/test2.py
+++ projet/test2.py
@@ -15,18 +15,18 @@ i = 0
 # use increments and thresholding for obstacles but index (64) for path
 
 for i in range(500):
-    print(i)
     map[i][150] = 64 # path
     map[i][100] = 255 # obstacle
     map[i][200] = 255 # obstacle
     # Makes it into grayscale
     graymap = map.astype('uint8')
     # change colormap for more pzazz (si les valeurs sont au dessus de 255 ca reboucle)
-    colormap = cv2.applyColorMap(graymap,  cv2.COLORMAP_JET);
+    colormap = cv2.applyColorMap(graymap,  cv2.COLORMAP_JET)
     # Resize for better viewing (dimensions seems to be inverted)
-    imS = cv2.resize(colormap, (map.shape[1]*2, map.shape[0]*2))
+    imS = cv2.resize(colormap, (map.shape[1]*2, map.shape[0]))
     cv2.imshow('image', imS)
     cv2.waitKey(1)
+    print(i)
 cv2.waitKey(0) # the image stays displayed as long as there is no imput but the program also stops
 # save image here
 print("test")
--- projet/zone_test_slam.py
+++ projet/zone_test_slam.py
@@ -7,6 +7,7 @@ import time
 import datetime as dt
 from threading import Event
 import numpy as np
+import cv2
 
 # Libraries crazyflie
 import cflib.crtp
@@ -23,36 +24,69 @@ map_shape = (500, 300)
 map = np.zeros(map_shape)
 
 offset_y = np.floor(map_shape[1]/2)
+# creer un offset_x pour le projet final
 
-plt.ion()
-fig = plt.figure()
 
 def fly_while_slam(drone:Easytrace):
     fly = True
     range_sensors = np.empty(5)
     position_estimate = [offset_y, 0]
 
+    thresh_obstcl = 50
+    blue = 64
+    red = 255
 
     #while fly:
-    for i in range(300):
+    for i in range(3000):
 
-        range_sensors[0] = drone.get_log('range.front')
-        range_sensors[1] = drone.get_log('range.back')
-        range_sensors[2] = drone.get_log('range.left')
-        range_sensors[3] = drone.get_log('range.right')
+        range_sensors[0] = np.floor(drone.get_log('range.front')/10)
+        range_sensors[1] = np.floor(drone.get_log('range.back')/10)
+        range_sensors[2] = np.floor(drone.get_log('range.left')/10)
+        range_sensors[3] = np.floor(drone.get_log('range.right')/10)
         range_sensors[4] = drone.get_log('range.up')
 
         position_estimate[0] = np.floor(drone.get_log('stateEstimate.x')*100)
         position_estimate[1] = offset_y + np.floor(drone.get_log('stateEstimate.y')*100)
 
-        map[int(position_estimate[0])][int(position_estimate[1])] = 1
+        print(i)
+        print(position_estimate)
 
-        if i%10 == 0:
-            plt.imshow(map)
-            fig.canvas.draw()
-            fig.canvas.flush_events()
+        # position mapping (should add limit cases)
+        map[int(position_estimate[0])][int(position_estimate[1])] = blue
 
-        drone.start_linear_motion(0.2, 0, 0)
+        # obstcale mapping
+        if position_estimate[0] + range_sensors[0] < map.shape[0]:
+            if range_sensors[0] < thresh_obstcl:
+                map[int(position_estimate[0] + range_sensors[0])][int(position_estimate[1])] = red  # front
+        if position_estimate[0] - range_sensors[1] > 0:
+            if range_sensors[1] < thresh_obstcl:
+                map[int(position_estimate[0] - range_sensors[1])][int(position_estimate[1])] = red  # back
+        if position_estimate[1] + range_sensors[2] < map.shape[1]:
+            if range_sensors[2] < thresh_obstcl:
+                map[int(position_estimate[0])][int(position_estimate[1] + range_sensors[2])] = red  # left
+        if position_estimate[1] - range_sensors[3] > 0:
+            if range_sensors[3] < thresh_obstcl:
+                map[int(position_estimate[0])][int(position_estimate[1] - range_sensors[3])] = red  # right
+
+        graymap = map.astype('uint8')
+        colormap = cv2.applyColorMap(graymap, cv2.COLORMAP_JET)
+        imS = cv2.resize(colormap, (map.shape[1], map.shape[0]))
+        cv2.imshow('image', imS)
+        cv2.waitKey(1)
+
+        # L shaped path for testing
+        if i <= 1000 :
+            drone.start_linear_motion(0.2, 0, 0)
+        elif i > 1000 and i <= 1500 :
+            drone.start_linear_motion(0, 0.2, 0)
+        elif i > 1500 and i <= 2000 :
+            drone.start_linear_motion(0, -0.2, 0)
+        else :
+            drone.start_linear_motion(-0.2, 0, 0)
+
+    drone.land()
+    # le drone se pose et l affichage du SLAM reste actif
+    cv2.waitKey(0)
 
 
 
