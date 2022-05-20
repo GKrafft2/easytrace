2f1e6aa8852d9edf6d3439a1b037c6cc2633192f
GKrafft2 <guillaume.krafft2@gmail.com>
2022-05-20 11:52:39 +0200

    Update slam.py

--- projet/slam.py
+++ projet/slam.py
@@ -8,13 +8,18 @@ from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.utils import uri_helper
 
+from drone import Drone
+
+from arena import Arena
+
 class Slam():
     def __init__(self):
         self.map_shape = (500, 300)
         self.map = np.zeros(self.map_shape)
-        self.offset_y = np.floor(self.map_shape[1] / 2)
+        self.offset_x = Arena.ORIGIN_X * 100
+        self.offset_y = Arena.ORIGIN_Y * 100
         self.range_sensors = np.empty(5)
-        self.position_estimate = [self.offset_y, 0]
+        self.position_estimate = [self.offset_x, self.offset_y]
         self.thresh_obstcl = 50
         self.blue = 128
         self.red = 255
@@ -22,11 +27,12 @@ class Slam():
     def update(self, range_sensor, position_estimates):
         self.range_sensors[0:4] = np.floor(range_sensor[0:4]/10)
         self.range_sensors[4] = range_sensor[4]
-        self.position_estimate[0] = np.floor(position_estimates[0] * 100)
+        self.position_estimate[0] = self.offset_x + np.floor(position_estimates[0] * 100)
         self.position_estimate[1] = self.offset_y + np.floor(position_estimates[1] * 100)
 
-        # position mapping (should add limit cases)
-        self.map[int(self.position_estimate[0])][int(self.position_estimate[1])] = self.blue
+        # position mapping
+        if self.position_estimate[0] < self.map.shape[0] and self.position_estimate[1] < self.map.shape[1]:
+            self.map[int(self.position_estimate[0])][int(self.position_estimate[1])] = self.blue
 
         # obstcale mapping
         if self.position_estimate[0] + self.range_sensors[0] < self.map.shape[0]:
@@ -51,7 +57,8 @@ class Slam():
     def map_update(self):
         graymap = self.map.astype('uint8')
         colormap = cv2.applyColorMap(graymap, cv2.COLORMAP_JET)
-        imS = cv2.resize(colormap, (self.map.shape[1], self.map.shape[0]))
+        imS = cv2.resize(colormap, (self.map.shape[1], self.map.shape[0]))  # reshape
+        imS = cv2.rotate(imS, cv2.ROTATE_180)  # rotate
         cv2.imshow('image', imS)
         cv2.waitKey(1)
 
@@ -59,18 +66,18 @@ class Slam():
 #     range_sensors = np.empty(5)
 #     position_estimate = np.empty(2)
 #     for i in range(3000):
-
+#
 #         range_sensors[0] = drone.get_log('range.front')
 #         range_sensors[1] = drone.get_log('range.back')
 #         range_sensors[2] = drone.get_log('range.left')
 #         range_sensors[3] = drone.get_log('range.right')
 #         range_sensors[4] = drone.get_log('range.up')
-
+#
 #         position_estimate[0] = drone.get_log('stateEstimate.x')
 #         position_estimate[1] = drone.get_log('stateEstimate.y')
-
-#         slam.slam_update(range_sensors, position_estimate)
-
+#
+#         slam.update(range_sensors, position_estimate)
+#
 #         # L shaped path for testing
 #         if i <= 1000 :
 #             drone.start_linear_motion(0.2, 0, 0)
@@ -80,10 +87,10 @@ class Slam():
 #             drone.start_linear_motion(0, -0.2, 0)
 #         else :
 #             drone.start_linear_motion(-0.2, 0, 0)
-
+#
 #     drone.land()
 #     # le drone se pose et l affichage du SLAM reste actif
-#     slam.slam_hold()
+#     slam.hold()
 
 
 
@@ -95,17 +102,17 @@ if __name__ == '__main__':
 
     with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
         # crée un drone (hérite de motion commander)
-        # drone = Drone(scf, default_height=0.2)
+        drone = Drone(scf, default_height=0.2)
         slam = Slam()
 
-        # drone.start_logs()
+        drone.start_logs()
 
-        # drone.take_off()
+        drone.take_off()
 
-        # fly_while_slam(drone)                  #without the slam class
-        # fly_while_slam_class(drone, slam)     # with the slam class
+        #fly_while_slam(drone)                  #without the slam class
+        fly_while_slam_class(drone, slam)     # with the slam class
 
-        # print("land")
-        # drone.land()
+        print("land")
+        drone.land()
 
         # drone.stop_logs(save=False)
